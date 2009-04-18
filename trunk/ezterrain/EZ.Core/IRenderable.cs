004cac85ccc8d653becb003f2d68e00bdd26987a
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-04-18 14:48:49 +0000

    kamera ve renderable gibi siniflarin oldugu core kutuphanesi eklendi

---
+++ trunk/ezterrain/EZ.Core/IRenderable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EZ.Core
+{
+	public interface IRenderable
+	{
+		bool Initialized { get; }
+
+		void Initialize();
+
+		bool Update(RenderInfo info);
+
+		void Render(RenderInfo info);
+	}
+}
---
+++ trunk/ezterrain/EZ.Core/ViewerInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Math;
+
+namespace EZ.Core
+{
+	public struct ViewerInfo
+	{
+		public ViewerInfo(Attitude attitude, Vector3 position, Perspective perspective)
+		{
+			this.attitude = attitude;
+			this.position = position;
+			this.perspective = perspective;
+		}
+
+		private Attitude attitude;
+		public Attitude Attitude
+		{
+			get { return attitude; }
+		}
+
+		private Vector3 position;
+		public Vector3 Position
+		{
+			get { return position; }
+		}
+
+		private Perspective perspective;
+		public Perspective Perspective
+		{
+			get { return perspective; }
+		}
+	}
+}
