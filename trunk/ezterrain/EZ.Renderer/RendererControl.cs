a776d41935d7dc5134beb189a03be1bf37693900
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-04-18 09:18:44 +0000

    renderer projesi eklendi

---
+++ trunk/ezterrain/EZ.Renderer/RendererControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenTK;
+
+namespace EZ.Renderer
+{
+	public partial class RendererControl : GLControl
+	{
+		public RendererControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
