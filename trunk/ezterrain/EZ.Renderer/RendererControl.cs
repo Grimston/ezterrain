f709e569836ed990d5051eb135b7cc16db05fdfc
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-04-18 10:07:50 +0000

    - RendererControl'da ClearColor ve Viewport duzenlemesi eklendi - MainForm'a renderer control eklendi(ilgili referanslar da projeye eklendi)

--- trunk/ezterrain/EZ.Renderer/RendererControl.cs
+++ trunk/ezterrain/EZ.Renderer/RendererControl.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using OpenTK;
+using OpenTK.Graphics;
 
 namespace EZ.Renderer
 {
@@ -15,6 +16,58 @@ namespace EZ.Renderer
 		public RendererControl()
 		{
 			InitializeComponent();
+		}
+
+		#region Load
+		protected bool Loaded { get; private set; }
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			Loaded = true;
+
+			SetClearColor();
+		}
+		#endregion
+
+		#region ClearColor
+		protected override void OnBackColorChanged(EventArgs e)
+		{
+			base.OnBackColorChanged(e);
+
+			SetClearColor();
+		}
+
+		private void SetClearColor()
+		{
+			if (Loaded)
+			{
+				GL.ClearColor(BackColor);
+			}
+		}
+		#endregion
+
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+
+			if (Loaded && Width > 0 && Height > 0)
+			{
+				GL.MatrixMode(MatrixMode.Projection);
+				GL.LoadIdentity();
+				GL.Viewport(0, 0, Width, Height);
+			}
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			GL.MatrixMode(MatrixMode.Modelview);
+			GL.LoadIdentity();
+
+
+			SwapBuffers();
 		}
 	}
 }
--- trunk/ezterrain/Ez.Application/MainForm.Designer.cs
+++ trunk/ezterrain/Ez.Application/MainForm.Designer.cs
@@ -28,13 +28,33 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
+			this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+			this.rendererControl = new EZ.Renderer.RendererControl();
 			this.SuspendLayout();
+			// 
+			// refreshTimer
+			// 
+			this.refreshTimer.Enabled = true;
+			this.refreshTimer.Interval = 10;
+			this.refreshTimer.Tick += new System.EventHandler(this.refreshTimer_Tick);
+			// 
+			// rendererControl
+			// 
+			this.rendererControl.BackColor = System.Drawing.Color.LightBlue;
+			this.rendererControl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.rendererControl.Location = new System.Drawing.Point(0, 0);
+			this.rendererControl.Name = "rendererControl";
+			this.rendererControl.Size = new System.Drawing.Size(784, 562);
+			this.rendererControl.TabIndex = 0;
+			this.rendererControl.VSync = true;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(784, 562);
+			this.Controls.Add(this.rendererControl);
 			this.Name = "MainForm";
 			this.Text = "Application";
 			this.ResumeLayout(false);
@@ -42,6 +62,9 @@
 		}
 
 		#endregion
+
+		private EZ.Renderer.RendererControl rendererControl;
+		private System.Windows.Forms.Timer refreshTimer;
 	}
 }
 
