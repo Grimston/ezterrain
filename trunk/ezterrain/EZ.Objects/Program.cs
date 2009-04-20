8fc11f9ae196efa60249c230f11c45b2f13dbd71
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-04-20 19:27:04 +0000

    program ve shader siniflari eklendi

---
+++ trunk/ezterrain/EZ.Objects/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Graphics;
+
+namespace EZ.Objects
+{
+	public class Program : Disposable
+	{
+		public bool Initialized { get; private set; }
+
+		public int Handle { get; private set; }
+
+		private Shader vertexShader;
+		private Shader fragmentShader;
+
+		public void Initialize(Shader vertexShader, Shader fragmentShader)
+		{
+			if (!Initialized)
+			{
+				this.vertexShader = vertexShader;
+				this.fragmentShader = fragmentShader;
+
+				if (!vertexShader.Initialized)
+				{
+					vertexShader.Initialize();
+				}
+
+				if (!fragmentShader.Initialized)
+				{
+					fragmentShader.Initialize();
+				}
+
+				Handle = GL.CreateProgram();
+
+				GL.AttachShader(Handle, vertexShader.Handle);
+				GL.AttachShader(Handle, fragmentShader.Handle);
+
+				GL.LinkProgram(Handle);
+
+				Initialized = true;
+			}
+		}
+
+		protected override void Dispose(bool nongc)
+		{
+			if (nongc)
+			{
+				if (Initialized)
+				{
+					GL.DetachShader(Handle, vertexShader.Handle);
+					GL.DetachShader(Handle, fragmentShader.Handle);
+
+					vertexShader.Dispose();
+					fragmentShader.Dispose();
+
+					vertexShader = null;
+					fragmentShader = null;
+
+					GL.DeleteProgram(Handle);
+
+					Handle = 0;
+					Initialized = false;
+				}
+			}
+		}
+	}
+}
---
+++ trunk/ezterrain/EZ.Objects/Shader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using OpenTK.Graphics;
+
+namespace EZ.Objects
+{
+	public class Shader : Disposable
+	{
+		public Shader(ShaderType type, string source)
+		{
+			this.Type = type;
+			this.Source = source;
+		}
+
+		public ShaderType Type { get; private set; }
+
+		public string Source { get; private set; }
+
+		public void LoadSource(string fileName)
+		{
+			using (StreamReader reader = new StreamReader(fileName))
+			{
+				Source = reader.ReadToEnd();
+			}
+		}
+
+		public static Shader FromFile(ShaderType type, string fileName)
+		{
+			Shader shader = new Shader(type, string.Empty);
+			shader.LoadSource(fileName);
+
+			return shader;
+		}
+
+		public bool Initialized { get; private set; }
+
+		public int Handle { get; private set; }
+
+		public void Initialize()
+		{
+			if (!Initialized)
+			{
+				Handle = GL.CreateShader(Type);
+
+				GL.ShaderSource(Handle, Source);
+
+				GL.CompileShader(Handle);
+
+				Initialized = true;
+			}
+		}
+
+		protected override void Dispose(bool nongc)
+		{
+			if (nongc)
+			{
+				GL.DeleteShader(Handle);
+
+				Initialized = false;
+				Handle = 0;
+			}
+		}
+	}
+}
