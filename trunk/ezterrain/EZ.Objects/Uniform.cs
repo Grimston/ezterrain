c7aab01b556a185734db15ac31c9f850f77406bf
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-04-23 19:57:34 +0000

    uniform sinifi eklendi

---
+++ trunk/ezterrain/EZ.Objects/Uniform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Graphics;
+
+namespace EZ.Objects
+{
+	public class Uniform
+	{
+		public Uniform(Program program, string name)
+		{
+			Location = GL.GetUniformLocation(program.Handle, name);
+		}
+
+		public int Location { get; private set; }
+
+		public void SetValue(int value)
+		{
+			GL.Uniform1(Location, value);
+		}
+
+		public void SetValue(float value)
+		{
+			GL.Uniform1(Location, value);
+		}
+	}
+}
