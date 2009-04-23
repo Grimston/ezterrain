d80bf09aa9b63d8b4796a11c48e4692d875ec34e
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-04-23 10:20:52 +0000

    kamera yonetiminde duzenleme yapildi

---
+++ ezterrain/EZ.Core/CoreExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Math;
+
+namespace EZ.Core
+{
+	public static class CoreExtensions
+	{
+		public static string ToString(this Vector3 vector, uint decimals)
+		{
+			string format = "({0:f#}, {1:f#}, {2:f#})".Replace("#", decimals.ToString());
+			return string.Format(format, vector.X, vector.Y, vector.Z);
+		}
+	}
+}
