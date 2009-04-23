e86768f0c98936b742dd954277f5b575bea68668
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-04-23 19:58:05 +0000

    ResourceManager sinifi eklendi. simdilik sadece path bilgisi veriyor

---
+++ trunk/ezterrain/EZ.Core/ResourceManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace EZ.Core
+{
+	public static class ResourceManager
+	{
+		public static readonly string ResourcePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Resources");
+
+		public static string GetProgramPath(string fileName)
+		{
+			return Path.Combine(Path.Combine(ResourcePath, @".\Programs"), fileName);
+		}
+
+		public static string GetImagePath(string fileName)
+		{
+			return Path.Combine(Path.Combine(ResourcePath, @".\Images"), fileName);
+		}
+	}
+}
