7fd83cf5a9d73576330767392a0e1ec8b3ac0597
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-04-26 12:22:03 +0000

    Vertex yapisi texturecoord barindiran ve barindirmayan olmak uzere iki sinif haline getirildi

---
+++ trunk/ezterrain/EZ.Clipmaps/VertexP.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Math;
+using System.Runtime.InteropServices;
+
+namespace Ez.Clipmaps
+{
+	[StructLayout(LayoutKind.Sequential)]
+	public struct VertexP
+	{
+		public VertexP(Vector3 position)
+		{
+			this.Position = position;
+		}
+
+		public VertexP(float x, float y, float z)
+		{
+			this.Position = new Vector3(x, y, z);
+		}
+
+		public Vector3 Position;
+
+		public override string ToString()
+		{
+			return string.Format("P{0}", Position);
+		}
+
+		public static readonly int SizeInBytes = Marshal.SizeOf(typeof(VertexPT));
+		public static readonly int PositionOffset = 0;
+	}
+}
---
+++ trunk/ezterrain/EZ.Clipmaps/VertexPT.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using OpenTK.Math;
+
+namespace Ez.Clipmaps
+{
+	[StructLayout(LayoutKind.Sequential)]
+	public struct VertexPT
+	{
+		public VertexPT(Vector3 position)
+		{
+			this.Position = position;
+			this.TexCoord = position.Xy;
+		}
+
+		public VertexPT(Vector3 position, Vector2 texCoord)
+		{
+			this.Position = position;
+			this.TexCoord = texCoord;
+		}
+
+		public Vector3 Position;
+		public Vector2 TexCoord;
+
+		public override string ToString()
+		{
+			return string.Format("P{0}T{1}", Position, TexCoord);
+		}
+
+		public static readonly int SizeInBytes = Marshal.SizeOf(typeof(VertexPT));
+		public static readonly int PositionOffset = 0;
+		public static readonly int TexCoordOffset = PositionOffset + Vector3.SizeInBytes;
+	}
+}
