a4646b835732c749ab8a3daf106f292e47684098
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-06-14 21:11:13 +0000

    lokalde yapilan toroidal update denemeleri projeye gecirilmeye baslandi

---
+++ trunk/ezterrain/EZ.Clipmaps/CopyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Ez.Clipmaps
+{
+	public struct CopyInfo
+	{
+		public CopyInfo(Point source, Point destination, Size size)
+		{
+			this.Source = source;
+			this.Destination = destination;
+			this.Size = size;
+		}
+
+		public Point Source;
+
+		public Point Destination;
+
+		public Size Size;
+
+		public Rectangle SourceRect
+		{
+			get { return new Rectangle(Source, Size); }
+		}
+
+		public Rectangle DestinationRect
+		{
+			get { return new Rectangle(Destination, Size); }
+		}
+	}
+}
---
+++ trunk/ezterrain/EZ.Clipmaps/TmpCopyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Ez.Clipmaps
+{
+	public struct TmpCopyInfo
+	{
+		public TmpCopyInfo(Point offset1, Point offset2, Size size)
+		{
+			this.Offset1 = offset1;
+			this.Offset2 = offset2;
+			this.Size = size;
+		}
+
+		public Point Offset1;
+
+		public Point Offset2;
+
+		public Size Size;
+
+		public Rectangle Rect1
+		{
+			get { return new Rectangle(Offset1, Size); }
+		}
+
+		public Rectangle Rect2
+		{
+			get { return new Rectangle(Offset2, Size); }
+		}
+	}
+}
