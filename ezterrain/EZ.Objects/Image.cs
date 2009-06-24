94a3e37bf68547d3f0bb35adcf3a07b30b394ec7
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-06-24 22:48:30 +0000

    - pixelformat helper static sinifi eklendi - pixelformat enumlari icin getcomponentcount fonksiyonu extension haline getirildi

--- ezterrain/EZ.Objects/Image.cs
+++ ezterrain/EZ.Objects/Image.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
-using OpenTK.Graphics;
 using System.ComponentModel;
+using OpenTK.Graphics;
 
 namespace EZ.Objects
 {
@@ -76,86 +76,7 @@ namespace EZ.Objects
 
 		public int ComponentCount
 		{
-			get { return GetComponentCount(PixelFormat); }
-		}
-
-		public static int GetComponentCount(PixelFormat pixelFormat)
-		{
-			switch (pixelFormat)
-			{
-				case PixelFormat.AbgrExt:
-				case PixelFormat.Bgra:
-				case PixelFormat.BgraInteger:
-				case PixelFormat.CmykExt:
-				case PixelFormat.CmykaExt:
-				case PixelFormat.DepthStencil:
-				case PixelFormat.Rgba:
-				case PixelFormat.RgbaInteger:
-					return 4;
-				case PixelFormat.Bgr:
-				case PixelFormat.BgrInteger:
-				case PixelFormat.DepthComponent:
-				case PixelFormat.Rgb:
-				case PixelFormat.RgbInteger:
-				case PixelFormat.Ycrcb444Sgix:
-					return 3;
-				case PixelFormat.LuminanceAlpha:
-				case PixelFormat.Rg:
-				case PixelFormat.RgInteger:
-				case PixelFormat.Ycrcb422Sgix:
-					return 2;
-				case PixelFormat.Alpha:
-				case PixelFormat.AlphaInteger:
-				case PixelFormat.Blue:
-				case PixelFormat.BlueInteger:
-				case PixelFormat.ColorIndex:
-				case PixelFormat.Green:
-				case PixelFormat.GreenInteger:
-				case PixelFormat.Luminance:
-				case PixelFormat.Red:
-				case PixelFormat.RedInteger:
-				case PixelFormat.StencilIndex:
-					return 1;
-				default:
-					throw new InvalidEnumArgumentException(pixelFormat.ToString());
-			}
-		}
-
-		public static int GetComponentCount(System.Drawing.Imaging.PixelFormat pixelFormat)
-		{
-			switch (pixelFormat)
-			{
-				case System.Drawing.Imaging.PixelFormat.Alpha:
-				case System.Drawing.Imaging.PixelFormat.PAlpha:
-				case System.Drawing.Imaging.PixelFormat.Format8bppIndexed:
-					return 1;
-				case System.Drawing.Imaging.PixelFormat.Format16bppGrayScale:
-				case System.Drawing.Imaging.PixelFormat.Format16bppRgb555:
-				case System.Drawing.Imaging.PixelFormat.Format16bppRgb565:
-				case System.Drawing.Imaging.PixelFormat.Format16bppArgb1555:
-					return 2;
-				case System.Drawing.Imaging.PixelFormat.Format24bppRgb:
-					return 3;
-				case System.Drawing.Imaging.PixelFormat.Canonical:
-				case System.Drawing.Imaging.PixelFormat.Format32bppArgb:
-				case System.Drawing.Imaging.PixelFormat.Format32bppPArgb:
-				case System.Drawing.Imaging.PixelFormat.Format32bppRgb:
-					return 4;
-				case System.Drawing.Imaging.PixelFormat.Format48bppRgb:
-					return 6;
-				case System.Drawing.Imaging.PixelFormat.Format64bppArgb:
-				case System.Drawing.Imaging.PixelFormat.Format64bppPArgb:
-					return 8;
-				case System.Drawing.Imaging.PixelFormat.Format1bppIndexed:
-				case System.Drawing.Imaging.PixelFormat.Extended:
-				case System.Drawing.Imaging.PixelFormat.Format4bppIndexed:
-				case System.Drawing.Imaging.PixelFormat.Gdi:
-				case System.Drawing.Imaging.PixelFormat.Indexed:
-				case System.Drawing.Imaging.PixelFormat.Max:
-				case System.Drawing.Imaging.PixelFormat.Undefined:
-				default:
-					throw new InvalidEnumArgumentException(pixelFormat.ToString());
-			}
+			get { return PixelFormat.GetComponentCount(); }
 		}
 	}
 }
