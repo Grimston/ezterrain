89e2334053011a94f63057e2ec566f52d8420b19
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-06-24 20:05:13 +0000

    image data siniflari olusturuldu

---
+++ trunk/ezterrain/EZ.Objects/Image1DData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EZ.Objects
+{
+	public class Image1DData<TPixel> : ImageData<TPixel>
+		where TPixel : struct, IPixel
+	{
+		public Image1DData(int width)
+			: base(width, 1, 1)
+		{ }
+
+		public new TPixel this[int column]
+		{
+			get { return base[column * PixelSize]; }
+			set { base[column * PixelSize] = value; }
+		}
+	}
+}
