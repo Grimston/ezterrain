7f3ff8af36aed28d020a26afa02dcd952c908b1d
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-06-24 22:38:03 +0000

    pixel tipleri ve opengl specific ozellikler eklendi

---
+++ ezterrain/EZ.Objects/PixelFormatAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Graphics;
+
+namespace EZ.Objects
+{
+	[global::System.AttributeUsage(AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+	sealed class PixelFormatAttribute : Attribute
+	{
+		public PixelFormatAttribute(PixelFormat pixelFormat)
+		{
+			this.PixelFormat = pixelFormat;
+		}
+
+		public PixelFormat PixelFormat { get; private set; }
+	}
+}
---
+++ ezterrain/EZ.Objects/PixelInternalFormatAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Graphics;
+
+namespace EZ.Objects
+{
+	[global::System.AttributeUsage(AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+	sealed class PixelInternalFormatAttribute : Attribute
+	{
+		public PixelInternalFormatAttribute(PixelInternalFormat pixelInternalFormat)
+		{
+			this.PixelInternalFormat = pixelInternalFormat;
+		}
+
+		public PixelInternalFormat PixelInternalFormat { get; private set; }
+	}
+}
