f4a89a4ed79f8ec23e0f2b19a645016ae6c101d4
zahir.tezcan <zahir.tezcan@c02875ce-246a-11de-8bc4-bb72633a7edc>
2009-07-21 10:58:10 +0000

    -terrain texture, image helper kullanir hale geldi -texture array kullanilir hale getirildi -renderer window icinde temel parametre olarak byte alignment 1 olarak duzenlendi

--- ezterrain/EZ.Clipmaps/TerrainTexture.cs
+++ ezterrain/EZ.Clipmaps/TerrainTexture.cs
@@ -4,30 +4,34 @@ using System.Linq;
 using System.Text;
 using EZ.Objects;
 using OpenTK.Graphics;
+using EZ.Core;
 
 namespace Ez.Clipmaps
 {
 	class TerrainTexture : TextureArray
 	{
 		#region GetImages
-		private static TerrainImageHelper[] GetImageHelpers(string sourceName, int maxLevel, int size)
+		private static TerrainImageHelper[] GetImageHelpers(string sourceFormat, int maxLevel, int size)
 		{
-			TerrainImageHelper[] imageHelpers = new TerrainImageHelper[maxLevel];
+			string format = ResourceManager.GetImagePath(sourceFormat);
+			TerrainImageHelper[] helpers = new TerrainImageHelper[maxLevel];
 
 			for (int i = 0; i < maxLevel; i++)
 			{
-				IImage wholeImage = ImageHelper.GetArrayImage(sourceName + i, i);
+				IImage wholeImage = ImageHelper.GetArrayImage(string.Format(format, i), i);
 				IImage gpuImage = wholeImage.NewImage(new Size3D(size, size, 1));
+
+				helpers[i] = new TerrainImageHelper(wholeImage, gpuImage);
 			}
 
-			return imageHelpers;
+			return helpers;
 		}
 
 		private static IImage[] GetImages(TerrainImageHelper[] helpers)
 		{
 			IImage[] images = new IImage[helpers.Length];
 
-			for (int i = 0; i < images.Length; i++, images[i] = helpers[i].GpuImage) ;
+			for (int i = 0; i < images.Length; images[i] = helpers[i].GpuImage, i++) ;
 
 			return images;
 		}
@@ -35,14 +39,18 @@ namespace Ez.Clipmaps
 
 		private TerrainImageHelper[] helpers;
 
-		public TerrainTexture(TextureUnit unit, int sideVertexCount, string sourceName, int maxLevel)
-			: this(unit, sideVertexCount, GetImageHelpers(sourceName, maxLevel, sideVertexCount))
+		public TerrainTexture(TextureUnit unit, int sideVertexCount, string sourceFormat, int maxLevels)
+			: this(unit, sideVertexCount, GetImageHelpers(sourceFormat, maxLevels, sideVertexCount))
 		{ }
 
 		private TerrainTexture(TextureUnit unit, int size, TerrainImageHelper[] helpers)
 			: base(unit, new Size2D(size, size), GetImages(helpers))
 		{
 			this.helpers = helpers;
+			this.MinFilter = TextureMinFilter.Nearest;
+			this.MagFilter = TextureMagFilter.Nearest;
+			this.WrapS = TextureWrapMode.ClampToBorder;
+			this.WrapT = TextureWrapMode.ClampToBorder;
 		}
 	}
 }
--- ezterrain/Resources/Programs/terrain.vert
+++ ezterrain/Resources/Programs/terrain.vert
@@ -4,15 +4,16 @@
 uniform float texScale;
 uniform float outer;
 
-const float level = outer + gl_InstanceID;
-const float levelScale = pow(2.0, level);
-
 in vec4 glVertex;
 
 varying out vec2 texCoord;
+varying out float level;
 
 void main()
 {
+	level = outer + float(gl_InstanceID);
+	float levelScale = pow(2.0, level);
+
 	texCoord = glVertex.xy * texScale + 0.5;
 
 	vec4 vertex = glVertex;
